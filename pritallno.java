50ed2c3dd7eda3ca32f588751508f49cad68439c
Riya kasaudhan <111666286+Riyasans@users.noreply.github.com>
2023-01-25 13:34:01 +0530

    Add files via upload

---
+++ pritallno.java
@@ -0,0 +1,17 @@
+import java.util.*; 
+public class pritallno{
+    public static void main(String args[]){
+        int num = 10;
+    
+        while(num<100){
+            if(num%2==0){
+             
+                System.out.print(" " + num); 
+              
+            }
+            num++;
+           
+        }
+        System.out.println();
+    }
+} 
